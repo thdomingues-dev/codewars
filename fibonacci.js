0bed1e5bc1df502555557b6a4c982ac1ab0fbd7a
Thales Domingues <thdomingues.dev@gmail.com>
2020-07-29 16:20:12 -0400

    codewars JS

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,8 @@
-function fibonacci(n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
+var hash = [0, 1];
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+function fibonacci(n) {
+  var current = n < hash.length ? hash[n] : fibonacci(n - 1) + fibonacci(n - 2);
+  return hash[n] = current, current;
 }
+
+console.log(fibonacci(5));
